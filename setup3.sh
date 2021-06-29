e2667953320d71004073db9b7dfa6dce06bedb62
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2021-06-29 23:58:36 +0100

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -31,7 +31,7 @@ mount /dev/$efipart /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_$hnme --recheck
 grub-mkconfig -o /boot/grub.cfg
 sed -i 's/#[multilib]/[multilib]/g /etc/pacman.conf
-sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g /etc/pacman.conf
+sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/g' /etc/pacman.conf
 
 
 echo reboot PC and run app install script 
