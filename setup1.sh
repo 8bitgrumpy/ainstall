e6e5ed55d98ae0f916305d4fbb90a793ddeb5100
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2021-06-29 23:47:33 +0100

    Add files via upload

--- setup1.sh
+++ setup1.sh
@@ -6,13 +6,6 @@ select G for GPT
 EFI partion size should be 512M
 dont bother with sap if over 16GB of ram . 
 
-Make filesystem example:
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 /dev/sda3
-
-
 mount /dev/mainuserpartition /mnt
 run the packstrap script.
 
