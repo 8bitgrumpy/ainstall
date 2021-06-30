fb31436847020f8d6a1d7eeb52f1e6cc112ac3d5
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2021-06-30 06:59:17 +0100

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -2,8 +2,11 @@
 
 
 pacstrap /mnt base linux-firmware linux-zen git
-genfstab -U /mnt >> /mnt/etc/fstab 
+genfstab -U /mnt >> /mnt/etc/fstab
+mkdir /mnt/tmp/ainstall
+cp *.* /mnt/tmp/ainstall
+arch-chroot /mnt
 echo -e "\e[36m
-echo now arch-chroot /mnt and run the setup script 
+echo Your not logged into the new install , scripts located at /tmp/ainstall
 \e[0m"
 
