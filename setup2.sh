a9e8c52e60a5db513e1d3bb272a03a24b997fa23
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2021-06-30 07:18:07 +0100

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -3,9 +3,8 @@
 
 pacstrap /mnt base linux-firmware linux-zen git
 genfstab -U /mnt >> /mnt/etc/fstab
-mkdir /mnt/tmp/ainstall
-cp *.* /mnt/tmp/ainstall
 echo -e "\e[36m
-echo Your not logged into the new install , scripts located at /tmp/ainstall
+echo Your not logged into the new install ,
+You will need to re-download the scripts ideally to /tmp using git clone https://github.com/8bitgrumpy/ainstall
 \e[0m"
 arch-chroot /mnt
