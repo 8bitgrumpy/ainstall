6a6b6d24460f1f962fced63822f4d6790b81ed6c
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2021-06-30 23:48:54 +0100

    Add files via upload

--- setup1.sh
+++ setup1.sh
@@ -1,22 +1,75 @@
-chmod +x setup2.sh
-chmod +x setup3.sh
-chmod +x setup4.sh
+#!/bin/bash
 echo -e "\e[36m
 
-
-Setup Disk Partitions use "fdisk"
-select G for GPT
-EFI partion size should be 512M
-dont bother with sap if over 16GB of ram . 
-
-
-Make filesystem example :
-mkfs.fat -F32 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mkfs.ext4 /dev/sda3
-
-mount /dev/mainuserpartition /mnt
-run the packstrap script.
+------------------------------------------
+        BS's Arch Installer 
+------------------------------------------
 
 \e[0m"
+echo -e "\e[33m  Enter Username to be created:\e[0m"
+read -p ":" newuname
+echo -e "\e[33m  Enter password:-\e[0m"
+read -s newunamepwd1
+echo -e "\e[33m  Enter password again:-\e[0m"
+read -s newunamepwd2
+if [ "$newunamepwd1" != "$newunamepwd2" ] ;
+then
+echo -e "\e[31m  Passwords do not match, re-run the script and try again !!!\e[0m"
+exit
+else 
+echo -e "\e[32m  Passwords match. Will use the same password for root account !!!\e[0m"
+fi
+echo -e "\e[33m  select disk:-\e[0m"
+dselect=($(lsblk -nd --output NAME))
+
+
+PS3="Select Drive:"
+driveschoice=("Drive 1 ${dselect[0]}" "Drive 2 ${dselect[1]}" "Drive 3 ${dselect[2]}" )
+select dselected in "${driveschoice[@]}"
+do
+case $dselected in
+"Drive 1 ${dselect[0]}")
+dinstall="${dselect[0]}"
+break
+;;
+"Drive 2 ${dselect[1]}")
+dinstall="${dselect[1]}"
+break
+;;
+"Drive 3 ${dselect[2]}")
+dinstall="${dselect[2]}"
+break
+;;
+*) ;;
+esac
+done
+echo -e "\e[32m  Drive \e[0m \e[31m---"$dinstall"---\e[0m \e[32m selected. All data will be wiped!\e[0m"
+read -p " Press w to continue. Any other key will exit this script. "
+if [ "$REPLY" != "w" ]; then
+exit
+fi
+echo -e "\e[31m  Drive "$dinstall" set to be wipped . No going back \e[0m"
+dd if=/dev/zero of /dev/sda bs=1M status=progress
+
+echo "
+g
+n
+1
+2048
++550M
+t
+1
+n
+2
+
++6G
+t
+2
+19
+n
+3
+
+
+w" | fdisk /dev/"$dinstall"
+
+
