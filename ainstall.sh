c1515df44969167674d3ac53710659fed92011fa
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2021-07-03 08:47:04 +0100

    Update ainstall.sh

--- ainstall.sh
+++ ainstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 echo -e "\e[36m
-localectl set-keymap --no-convert uk
 ------------------------------------------
         Brendan's Luxury Arch Installer 
 ------------------------------------------
-
 \e[0m"
+
+
+#Set username + hostname var
+
 echo -e "\e[33m  Enter Hostname to be created:\e[0m"
 read -p ":" hname
 echo -e "\e[33m  Enter Username to be created:\e[0m"
@@ -14,6 +16,8 @@ echo -e "\e[33m  Enter password:-\e[0m"
 
 echo -e "\e[33m  select disk:-\e[0m"
 dselect=($(lsblk -nd --output NAME))
+
+# set install destination var 
 
 
 PS3="Select Drive:"
@@ -36,6 +40,7 @@ break
 *) ;;
 esac
 done
+# set processor var
 PS3="Select Processor:"
 cpuc=("AMD" "INTEL")
 select cpuselected in "${cpuc[@]}"
@@ -60,6 +65,7 @@ else
 ucode="intel-ucode"
 fi
 
+# confirm before instal
 
 echo -e "\e[32m  Username:  =  \e""[0m \e[31m"$newuname"\e[0m"
 echo -e "\e[32m  Drive to be wiped : =  \e""[0m \e[31m"$dinstall"\e[0m"
@@ -70,8 +76,9 @@ if [ "$REPLY" != "w" ]; then
 exit
 fi
 echo -e "\e[31m  Drive "$dinstall" set to be wipped . No going back \e[0m"
+#wipe selected drive
 dd if=/dev/zero of=/dev/"$dinstall" bs=1M status=progress
-
+# create and format partitions 
 echo "
 g
 n
@@ -97,12 +104,16 @@ mkswap /dev/"$dinstall"2
 swapon /dev/"$dinstall"2
 mkfs.ext4 /dev/"$dinstall"3
 echo -e "\e[33m  Drives Created & formatted. Running packstrap :\e[0m"
+# pacstrap base components 
 mount /dev/"$dinstall"3 /mnt
 pacstrap /mnt base linux-firmware linux-zen git
+# createing fstab to mount drives on boot
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# creating second stage install script based on var's set above
 cat > /mnt/setup2.sh <<EOF
-
+# --- second stage install script #
+# install base components for sudo , bootloader and shell
 pacman -Sy --noconfirm \
 sudo \
 grub \
@@ -112,37 +123,47 @@ nano \
 $ucode \
 zsh \
 grml-zsh-config 
-
+# set user passwords
 echo -e "\e[32m  Creating user:  =  \e""[0m \e[31m"$newuname"\e[0m"
 useradd -m "$newuname"
 passwd "$newuname"
 echo -e "\e[32m  Set password for:  =  \e""[0m \e[31m"root"\e[0m"
 passwd
-systemctl enable NetworkManager
+# add user to default groups
 usermod -aG wheel,audio,video,optical,storage $newuname
+# set sudo to no password
 sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
+# set timezone
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 Set timezone
 hwclock --systohc
+# set hosts and localhost files 
 sed -i '$ a 127.0.0.1 localhost' /etc/hosts
 sed -i '$ a ::1 localhost' /etc/hosts
 sed -i "$ a 127.0.0.1 $hname" /etc/hosts
 cat /etc/hosts
 echo "$hname" > /etc/hostname
 cat /etc/hostname
+# set locale
 sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
+# create bootloader
 mkdir /boot/EFI
 mount /dev/"$dinstall"1 /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=arch_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 timedatectl set-ntp true
+# set to uk keymap 
 echo "KEYMAP=uk" > /etc/vconsole.conf
+# change default shell to zsh 
+echo -e "\e[32m  To set ZSH as default shell enter password for:  =  \e""[0m \e[31m"$newuname"\e[0m"
 chsh -s $(which zsh)
+# enable services 
+systemctl enable NetworkManager
 EOF
 
-
+# copy generated second stage script to chroot , run + tidyup. 
 chmod +x /mnt/setup2.sh
 arch-chroot /mnt ./setup2.sh
 rm /mnt/setup2.sh
